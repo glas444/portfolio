3ed2c506ba73944a46bfa78b9dac38063c31af06
Andreas Wrife <awrife@kth.se>
2024-02-14 15:42:24 +0100

    about Me _ kth _ main updated text

--- js/main1.js
+++ js/main1.js
@@ -60,6 +60,16 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+window.onload = function () {
+  // Hardcode the birth year
+  var birthYear = 1999;
+  // Calculate the age
+  var currentYear = new Date().getFullYear();
+  var age = currentYear - birthYear;
+  // Update the age in the HTML
+  document.getElementById("age").innerText = age;
+};
+
 document.getElementById("fluidsim").addEventListener("mouseenter", function () {
   fluidsimVideo.play();
 });
