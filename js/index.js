a93e29f9c1c12e761795db65f0f29d399a88ce7f
Andreas Wrife <awrife@kth.se>
2024-02-21 18:29:10 +0100

    added mobile hover videos function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
-document.getElementById("fluidsim").addEventListener("mouseenter", function () {
+document
+  .getElementById("fluidsim")
+  .addEventListener("mouseenter", playFluidVideo);
+document
+  .getElementById("fluidsim")
+  .addEventListener("touchstart", playFluidVideo);
+function playFluidVideo() {
   fluidsimVideo.play();
-});
+}
 
-document.getElementById("fluidsim").addEventListener("mouseleave", function () {
+document
+  .getElementById("fluidsim")
+  .addEventListener("mouseleave", pauseFluidVideo);
+document
+  .getElementById("fluidsim")
+  .addEventListener("touchend", pauseFluidVideo);
+function pauseFluidVideo() {
   fluidsimVideo.pause();
-  // fluidsimVideo.currentTime = 0;
-});
+}
 
-document.getElementById("pp").addEventListener("mouseenter", function () {
+document.getElementById("pp").addEventListener("mouseenter", playPPVideo);
+document.getElementById("pp").addEventListener("touchstart", playPPVideo);
+function playPPVideo() {
   document
     .getElementById("ppVideo")
     .setAttribute("src", "images/pp_banner5.gif");
-});
+}
 
-document.getElementById("pp").addEventListener("mouseleave", function () {
+document.getElementById("pp").addEventListener("mouseleave", pausePPVideo);
+document.getElementById("pp").addEventListener("touchend", pausePPVideo);
+function pausePPVideo() {
   document
     .getElementById("ppVideo")
     .setAttribute("src", "images/pp_banner4.gif");
-});
-// Overflow Menu
+}
